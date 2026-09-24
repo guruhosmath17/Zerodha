b6c80a57e6a8b7f733a5deef08e7e82cc4805c3e
guru <guruhosmath18@gmail.com>
2026-09-24 20:50:27 +0530

    Fix frontend routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import SupportPage from './landing_page/support/SupportPage';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/Foooter';
 import NotFound from './landing_page/NotFound';
+import LoginPage from './landing_page/login/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
